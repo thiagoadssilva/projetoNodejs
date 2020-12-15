d2e09a7628834e6b5d97ca69e91e9e71492269d9
thiago jose da silva <thiago.ads.silva@gmail.com>
2020-12-14 22:59:04 -0300

    helpers

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router = require('./routes/index');
+const helpers = require('./helpers');
 const mustach = require('mustache-express');
-
 const app = express();
+
+app.use((req, res, next) =>{
+    res.locals.h = helpers;
+    next();
+}); 
+
+
 app.use('/', router);
 app.use(express.json());
 
--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,12 @@ const express = require('express');
 
 const router = express.Router();
 router.get('/', (req, res) => {
-    res.render('home', {
-        nome: req.query.nome,
-    });
+    
+    let obj = {
+        pageTitle: "Thiago jose"
+    };
+    
+    res.render('home', obj);
 });
 
  

833ae4794260a8b68546d7a83d4d1ae4164bf9a7
thiago jose da silva <thiago.ads.silva@gmail.com>
2020-12-20 14:10:22 -0300

    Listando os posts

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,7 +1,14 @@
-exports.index = (req, res) => {
-    let obj = {
+const mongoose = require('mongoose');
+const Post = mongoose.model('Post');
+
+exports.index = async (req, res) => {
+    let responseJson = {
         pageTitle: "Thiago jose",
-        userInfo: req.userInfo
-    }; 
-    res.render('home', obj); 
+        posts:[]
+    };
+    
+    const posts = await Post.find();
+    responseJson.posts = posts; 
+    
+    res.render('home', responseJson); 
 };
